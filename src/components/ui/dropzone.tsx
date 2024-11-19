17d1e661849b5299b32fc24abc1fffbc79c48c62
rafaeelricco <rafaelricco10@gmail.com>
2024-11-19 19:24:22 -0300

    feat: add react-dropzone package and implement Dropzone component
    This commit adds the react-dropzone package to the project dependencies and implements the Dropzone component in the src/components/ui/dropzone.tsx file. The Dropzone component allows users to upload files by either clicking on the component or dragging and dropping files onto it. It includes visual feedback for focus, accepted file types, and rejected file types.

---
+++ src/components/ui/dropzone.tsx
@@ -0,0 +1,50 @@
+import { cn } from '@/src/lib/utils'
+import { CloudUpload } from 'lucide-react'
+import { useDropzone } from 'react-dropzone'
+
+import React from 'react'
+
+const Dropzone: React.FC<DropzoneProps> = ({ onDropFiles }: DropzoneProps) => {
+   const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
+      useDropzone({
+         accept: { 'application/pdf': [] },
+         onDrop: (acceptedFiles) => {
+            console.log('acceptedFiles', acceptedFiles)
+            onDropFiles(acceptedFiles)
+         }
+      })
+
+   return (
+      <div
+         className={cn(
+            'flex h-auto w-full flex-1 border-spacing-1 flex-col items-center justify-center rounded-lg border border-dashed border-white-500 bg-transparent py-8 transition-all duration-300',
+            {
+               'border-primary-default': isFocused,
+               'border-green-500': isDragAccept,
+               'border-red-500': isDragReject
+            }
+         )}
+         {...getRootProps({ isFocused, isDragAccept, isDragReject })}
+      >
+         <input {...getInputProps()} />
+         <div className="flex flex-1 flex-col items-center justify-center">
+            <CloudUpload className="my-2 h-8 w-8 text-black-500" />
+            <p className="font-sf-pro-display text-base">
+               <span className="cursor-default font-sf-pro-display font-semibold underline">
+                  Clique para enviar
+               </span>{' '}
+               ou arraste e solte
+            </p>
+            <p className="font-sf-pro-display text-sm text-white-800">
+               Tamanho máximo de 2GB.
+            </p>
+         </div>
+      </div>
+   )
+}
+
+type DropzoneProps = {
+   onDropFiles: (files: File[]) => void
+}
+
+export { Dropzone }
